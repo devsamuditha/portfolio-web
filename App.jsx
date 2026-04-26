b9437d7e9cb8e8bd235684d1fec44d81400aa4cd
shehan samuditha <dev.samuditha@gmail.com>
2026-04-26 11:58:01 +0530

    update-1.2

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Project from './components/Project'
+
+const App = () => {
+  return (
+    <div>
+        <Navbar />
+        < Hero />
+        < Project />
+    </div>
+  )
+}
+
+export default App
